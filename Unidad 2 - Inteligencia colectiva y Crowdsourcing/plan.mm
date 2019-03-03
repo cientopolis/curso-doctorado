11c73f2255efcab66ca2172a780aaf06e55a30d6
Diego Torres <dtorres@MacBook-Pro-13-de-Towers.local>
2019-03-03 20:30:04 -0300

    el resto de las unidades

---
+++ Unidad 2 - Inteligencia colectiva y Crowdsourcing/plan.mm
@@ -0,0 +1,156 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Collective intelligence for Citizen Science" FOLDED="false" ID="ID_365668024" CREATED="1511784470942" MODIFIED="1511796290831" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Topics" POSITION="right" ID="ID_1980432332" CREATED="1511784483837" MODIFIED="1511784490328">
+<edge COLOR="#ff0000"/>
+<node TEXT="Can research be described as collaborative work?" ID="ID_482513421" CREATED="1511784490668" MODIFIED="1511784533335"/>
+<node TEXT="How do CI systems work?" ID="ID_967511196" CREATED="1511784496436" MODIFIED="1511784503207"/>
+</node>
+<node TEXT="Contents" POSITION="right" ID="ID_1579126499" CREATED="1511784504916" MODIFIED="1511784652501">
+<edge COLOR="#0000ff"/>
+<node TEXT="Genome of collective intelligence" ID="ID_698094461" CREATED="1511784653106" MODIFIED="1511785053853"/>
+<node TEXT="Collaborative work" ID="ID_1271797590" CREATED="1511784667410" MODIFIED="1511784676316"/>
+<node TEXT="Computer support for collaborative work" ID="ID_1138273423" CREATED="1511784676978" MODIFIED="1511784687204"/>
+<node TEXT="CI and AI" ID="ID_119883396" CREATED="1511784749961" MODIFIED="1511784768019"/>
+</node>
+<node TEXT="Approach ideas" POSITION="right" ID="ID_1570431370" CREATED="1511784723841" MODIFIED="1511784729500">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Cerrar con un caso de citizen science" ID="ID_220317377" CREATED="1511784729921" MODIFIED="1511784741420"/>
+<node TEXT="Ojo que cuando vemos un proyecto a travez de una de sus caras (por ejemplo, app web) solo tenemos acceso a algunas de las actividades que lo componen." ID="ID_829479699" CREATED="1511785183028" MODIFIED="1511785216489"/>
+<node TEXT="Mostrar la diferencia entre Mechanical turk (y concursos) dependencia en Money, mientras que wikipedia y otros dependen on glory and love." ID="ID_317694399" CREATED="1511785428473" MODIFIED="1511785470099"/>
+<node TEXT="Orden?" ID="ID_1884980008" CREATED="1511785568607" MODIFIED="1511785648521">
+<node TEXT="Colaboraci&#xf3;n -&gt; Inteligencia colectiva -&gt; CSCW ?" ID="ID_456339294" CREATED="1511785648926" MODIFIED="1511785650265"/>
+<node TEXT="Colabroaci&#xf3;n -&gt; Colaboraci&#xf3;n en la ciencia -&gt; CSCW -&gt; Inteligencia colectiva -&gt; Inteligencia colectiva en la ciencia" ID="ID_1825839795" CREATED="1511785650694" MODIFIED="1511785684152"/>
+</node>
+<node TEXT="Iniciativas que mostrar" ID="ID_812221156" CREATED="1511785927635" MODIFIED="1511785933325">
+<node TEXT="InnoCentive" ID="ID_1351871059" CREATED="1511785933723" MODIFIED="1511785936053"/>
+<node TEXT="Threadless" ID="ID_39680902" CREATED="1511785952947" MODIFIED="1511785958125"/>
+<node TEXT="Netflix price" ID="ID_1338882299" CREATED="1511785978043" MODIFIED="1511785981188"/>
+<node TEXT="Top Coder" ID="ID_125124526" CREATED="1511785991658" MODIFIED="1511785993804"/>
+<node TEXT="IBM Innovation jams" ID="ID_871017510" CREATED="1511785994106" MODIFIED="1511785998228"/>
+<node TEXT="Chequeado o algo de fake news" ID="ID_361452663" CREATED="1511787130476" MODIFIED="1511787144319"/>
+<node TEXT="Amazon reviews" ID="ID_1751558019" CREATED="1511788286063" MODIFIED="1511788289065"/>
+<node TEXT="Prediction markets in choosing opensource projects" ID="ID_1068743769" CREATED="1511788289663" MODIFIED="1511788361632"/>
+<node TEXT="Gamification in Stackexchange" ID="ID_545820681" CREATED="1511788467437" MODIFIED="1511788513559"/>
+<node TEXT="NASA click workers" ID="ID_151099306" CREATED="1511794622427" MODIFIED="1511794626510"/>
+<node TEXT="http://book.openingscience.org/cases_recipes_howtos/how_this_book_was_created_using_collaborative_authoring.html" ID="ID_1424637582" CREATED="1511801305882" MODIFIED="1511801307646"/>
+</node>
+</node>
+<node TEXT="Orden" POSITION="left" ID="ID_665097337" CREATED="1511786191728" MODIFIED="1511786194490">
+<edge COLOR="#00ffff"/>
+<node TEXT="Alguna situaci&#xf3;n motivadora" ID="ID_695432112" CREATED="1511786239031" MODIFIED="1511786260282"/>
+<node TEXT="Collaborative work" ID="ID_1511453973" CREATED="1511786197368" MODIFIED="1511786203050"/>
+<node TEXT="Collaborative work en la ciencia" ID="ID_1808358629" CREATED="1511786203576" MODIFIED="1511786230614"/>
+<node TEXT="Computer supported collaborative work" ID="ID_1822697580" CREATED="1511786278903" MODIFIED="1511786300889">
+<node TEXT="Espacio tiempo" ID="ID_134009233" CREATED="1511786335566" MODIFIED="1511786344721"/>
+<node TEXT="Awareness" ID="ID_107658106" CREATED="1511786345278" MODIFIED="1511786351336"/>
+<node TEXT="Familias" ID="ID_1927961512" CREATED="1511786354086" MODIFIED="1511786362744"/>
+<node TEXT="Objetivo: entender que hay que hacer, y que esfuerzo implica" ID="ID_1294563463" CREATED="1511786385974" MODIFIED="1511786403272"/>
+<node TEXT="Why groupware fails" ID="ID_563083862" CREATED="1511786421309" MODIFIED="1511786425807"/>
+</node>
+<node TEXT="Inteligencia colectiva" ID="ID_1688092881" CREATED="1511786307015" MODIFIED="1511786310881">
+<node TEXT="Genoma" ID="ID_87476725" CREATED="1511786451109" MODIFIED="1511786466703"/>
+<node TEXT="Motivation is critical" ID="ID_986568422" CREATED="1511789679862" MODIFIED="1511789688945">
+<node TEXT="Incluir gamification" ID="ID_676326764" CREATED="1511788576932" MODIFIED="1511788581014"/>
+</node>
+<node TEXT="IC and AI" ID="ID_1565593801" CREATED="1511786469485" MODIFIED="1511786476335">
+<node TEXT="Entrenamiento" ID="ID_1077278160" CREATED="1511786548052" MODIFIED="1511786552598"/>
+<node TEXT="No pedir que la gente haga cosas que pueden hacer las computadoras ( -&gt; why groupware fails)" ID="ID_1316962475" CREATED="1511786553140" MODIFIED="1511786583390"/>
+</node>
+<node TEXT="Crows vs Communities" ID="ID_1298278064" CREATED="1511792539304" MODIFIED="1511792543860">
+<node TEXT="bird watchers vs cazamosquitos" ID="ID_1611171435" CREATED="1511793337445" MODIFIED="1511793354559"/>
+</node>
+<node TEXT="Crowd recruiting and sustainability" ID="ID_1945898976" CREATED="1511789710710" MODIFIED="1511792577306"/>
+</node>
+<node TEXT="Inteligencia colectiva en la ciencia" ID="ID_1368113367" CREATED="1511786320510" MODIFIED="1511786330928"/>
+</node>
+<node TEXT="Actividades" POSITION="left" ID="ID_956486385" CREATED="1511787102141" MODIFIED="1511787107223">
+<edge COLOR="#00007c"/>
+<node TEXT="Presentar un caso en detalle, y darles un framework de analisis de los aspectos de colaboraci&#xf3;n, de analisis de que no rompan &quot;wgf&quot;, y de complejidad en el soporte requerido." ID="ID_209375523" CREATED="1511787029398" MODIFIED="1511787109587">
+<node TEXT="Esto pueden incluirlo en el analisis que hagan de CC" ID="ID_958166302" CREATED="1511788605027" MODIFIED="1511788625109"/>
+</node>
+<node TEXT="Intentar analizar si se trata de comunicad o crowd - confirmar la existencia de una comunidad si ese fuera el caso." ID="ID_625128808" CREATED="1511796264418" MODIFIED="1511796287492"/>
+</node>
+</node>
+</map>
